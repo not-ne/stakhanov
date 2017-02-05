9bcaf96b3880baaa98de0be3d9db8dd73c924dfe
Rob <rwrobe@gmail.com>
2017-02-05 19:08:56 +0000

    Made the keepalive interval a process var

--- bin/bot.js
+++ bin/bot.js
@@ -2,16 +2,17 @@
 
 'use strict';
 
-var Stakhanov = require('../lib/stakhanov');
+var Stakhanov = require( '../lib/stakhanov' );
 var KeepAlive = require( '../lib/keepAlive' );
 
-var token = process.env.BOT_API_KEY || require('../token');
+var token = process.env.BOT_API_KEY || require( '../token' );
 var name = process.env.BOT_NAME;
+var ivl = process.env.KEEP_ALIVE_INTERVAL;
 
-var stakhanov = new Stakhanov({
-    token: token,
-    name: name
-});
+var stakhanov = new Stakhanov( {
+	token: token,
+	name: name
+} );
 
 stakhanov.run();
-KeepAlive.init( 300000 );
+KeepAlive.init( ivl );
